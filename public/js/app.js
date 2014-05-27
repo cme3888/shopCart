560c21985896bb336d55af6b4b8f5f3ade108eed
Your Name <you@example.com>
2014-05-27 08:04:41 -0400

    yup

--- public/js/app.js
+++ public/js/app.js
@@ -32,7 +32,7 @@ angular
         templateUrl: 'views/store-list.html',
         controller: 'StoreCtrl'
       })
-      .when('/newitem', {
+      .when('/newItem', {
         templateUrl: 'views/toy-create.html',
         controller: 'StoreCtrl'
 
